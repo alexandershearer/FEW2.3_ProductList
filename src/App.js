96735afbc9c932fc471107674650913f004111bf
alexandershearer <alex.shearer23@gmail.com>
2021-06-06 18:56:18 -0500

    State handling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
+import { useState } from 'react'
 import data, { categoryUnique } from './data'
 import ProductList from './ProductList'
 import CategoryList from './CategoryList'
 import Header from './Header'
 
 function App() {
+  const [category, setCategory] = useState('Books')
   return (
     <div className="App">
       <Header title="Productive" productCount={data.length} categoryCount={categoryUnique.length} />
-
-      <CategoryList />
-
-      <ProductList />
+      <CategoryList category={category} />
+      <ProductList category={category} />
     </div>
   );
 }
 
 export default App;
+
+
+/*
+
+- div.App <- Selected Category
+  - Header
+  - Category List
+    - div.CategoryList
+      -Button
+      -Button
+  - Product List
+    - div
+      - h2
+      - p
+      - p
+      - small
+
+
+*/
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,12 +1,16 @@
 import { catsAndCounts } from './data'
 import './CategoryList.css'
 
-function CategoryList() {
+function CategoryList(props) {
+
+    const { category } = props
+
     return (
         <div className="CategoryList">
             {catsAndCounts.map(obj => {
+                const className = obj.name === category ? 'selected' : ''
                 return (
-                    <button>
+                    <button className={className}>
                         {obj.name}
                         <span> {obj.count} </span>
                     </button>
